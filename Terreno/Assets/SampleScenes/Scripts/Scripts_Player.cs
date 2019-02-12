95448fb18059ba14e842cae6a63829bb5af6162c
saikara <saikaradark@hotmail.com>
2019-02-12 12:12:53 +0100

    Terrenos

---
+++ Terreno/Assets/SampleScenes/Scripts/Scripts_Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts_Player : MonoBehaviour{
+    [SerializeField] GameObject linterna;
+    [SerializeField] AudioSource audioSource;
+
+    void Update()
+    { if (Input.GetKeyDown(KeyCode.L)) {
+            linterna.SetActive(!linterna.activeSelf);//nueva forma de  hacerlo//
+        } 
+        else if (Input.GetButtonDown("Fire1")) {
+            audioSource.Play();
+
+        }
+        
+    }
+}
