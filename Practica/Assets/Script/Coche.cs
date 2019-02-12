5a61b46a0ffabae1c6a4e5ce3aa53c13dbbbf292
saikara <saikaradark@hotmail.com>
2019-02-12 12:10:15 +0100

    Practica

---
+++ Practica/Assets/Script/Coche.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coche : MonoBehaviour
+{
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+   
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+       
+            if (Mathf.Abs(steering)>0)
+            {
+                leftWheel.steerAngle = steering;
+                rightWheel.steerAngle = steering;
+            }
+            if (Mathf.Abs(motor)>0)
+            {
+                leftWheel.motorTorque = motor;
+                rightWheel.motorTorque = motor;
+            }
+        
+    }
+}
---
+++ Practica/Assets/Script/Oso.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oso : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(name);
+        print(collision.gameObject.name);
+    }
+}
