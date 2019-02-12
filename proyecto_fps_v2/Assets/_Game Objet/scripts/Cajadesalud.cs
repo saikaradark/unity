c1e08bdbac4dcdbd60852c874fd04118510adc1e
saikara <saikaradark@hotmail.com>
2019-02-12 14:02:10 +0100

    hola

---
+++ proyecto_fps_v2/Assets/_Game Objet/scripts/Cajadesalud.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cajadesalud : MonoBehaviour{
+
+    public int cantidadSalud = 50;
+    [SerializeField] private float velocidadGiro = 100f;
+
+    void Update(){
+        Rotar();
+   
+    }
+
+    private void Rotar() {
+        transform.Rotate(new Vector3(0, Time.deltaTime * velocidadGiro, 0));
+
+
+    }
+    private void OnTriggerEnter(Collider other) {
+        print("Ha entrado");
+        /*
+         * 1. la caja de salud a sido atravesada por el player?
+         * 
+         * 
+         */
+         if (other.gameObject.CompareTag("Player")) {
+            bool atope = other.gameObject.GetComponent<Player>().IncrementarSalud(cantidadSalud);
+            if (atope == false) {
+                Destroy(this.gameObject);
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ proyecto_fps_v2/Assets/_Game Objet/scripts/Proyectil_Enemigo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil_Enemigo : MonoBehaviour {
+    [SerializeField] int danyo;
+    private void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject.CompareTag("Player")) {
+            collision.gameObject.GetComponent<Player>().RecibirDanyo(danyo);
+            Destruir();
+        }
+    }
+    private void Destruir() {
+        Destroy(this.gameObject);
+    }
+}
